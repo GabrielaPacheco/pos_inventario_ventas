052b8dda4c2e47e3a1515b80614534387074b565
Gabriela Pacheco <ccgk02.gabriela.pacheco@gmail.com>
2022-10-23 21:51:02 -0600

    Agregando usuario hacia la base de datos
    Agregando usuario a base de datos funcional con mensaje de éxito y modificando parte de codigo de instancia a clase conexion en los métodos mdlIngresarUsuario y mdlMostrarUsuarios.

--- models/usuarios.model.php
+++ models/usuarios.model.php
@@ -8,10 +8,26 @@ class ModeloUsuarios
 
     static public function mdlMostrarUsuarios($tabla, $item, $valor)
     {
-        $stmt = (new Conexion)->conectar()->prepare("SELECT * FROM $tabla WHERE $item = :$item"); //SE DEBE DE ENLAZAR PARAMETRO $item
+        $stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla WHERE $item = :$item"); //SE DEBE DE ENLAZAR PARAMETRO $item
         $stmt->bindParam(":" . $item, $valor, PDO::PARAM_STR); //ENLAZANDO PARAMETRO
         $stmt->execute();
         return $stmt->fetch(); //RETORNANDO UN SOLO ITEM DE NUESTRA TABLA
-        
+    }
+
+    // REGISTRO DE USUARIO
+    static public function mdlIngresarUsuario($tabla, $datos)
+    {
+        $stmt = Conexion::conectar()->prepare("INSERT INTO $tabla (nombre, usuario, password, perfil) VALUES (:nombre, :usuario, :password, :perfil)"); //SE DEBE DE ENLAZAR PARAMETRO $item
+        $stmt->bindParam(":nombre", $datos["nombre"], PDO::PARAM_STR); //ENLAZANDO PARAMETROS
+        $stmt->bindParam(":usuario", $datos["usuario"], PDO::PARAM_STR); //ENLAZANDO PARAMETROS
+        $stmt->bindParam(":password", $datos["password"], PDO::PARAM_STR); //ENLAZANDO PARAMETROS
+        $stmt->bindParam(":perfil", $datos["perfil"], PDO::PARAM_STR); //ENLAZANDO PARAMETROS
+
+        if($stmt->execute()){
+            return "ok";
+        }
+        else{
+            return "error";
+        }
     }
 }
