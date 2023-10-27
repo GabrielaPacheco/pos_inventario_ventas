51adc7993a4c5c750c0a7f32e36f5db372030630
Gabriela Pacheco <ccgk02.gabriela.pacheco@gmail.com>
2023-10-27 17:56:59 -0600

    Actualizando venta completo
    Funcionamiento total del editar venta en modulo de ventas.

--- models/ventas.model.php
+++ models/ventas.model.php
@@ -8,12 +8,12 @@ class ModeloVentas
     {
         if ($item != null) {
             $stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla WHERE $item = :$item
-            ORDER BY fecha DESC"); //SE DEBE DE ENLAZAR PARAMETRO $item
+            ORDER BY id ASC"); //SE DEBE DE ENLAZAR PARAMETRO $item
             $stmt->bindParam(":" . $item, $valor, PDO::PARAM_STR); //ENLAZANDO PARAMETRO
             $stmt->execute();
             return $stmt->fetch(); //RETORNANDO UN SOLO ITEM DE NUESTRA TABLA
         } else {
-            $stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla");
+            $stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla ORDER BY id ASC");
             $stmt->execute();
             return $stmt->fetchAll(); //RETORNANDO TODOS LOS VALORES DE LA TABLA
         }
@@ -48,4 +48,33 @@ class ModeloVentas
         }
         $stmt = null;
     }
+    /*=============================================
+	EDITAR VENTA
+	=============================================*/
+
+	static public function mdlEditarVenta($tabla, $datos){
+
+		$stmt = Conexion::conectar()->prepare("UPDATE $tabla SET  id_cliente = :id_cliente, id_vendedor = :id_vendedor, productos = :productos, impuesto = :impuesto, neto = :neto, total= :total, metodo_pago = :metodo_pago WHERE codigo = :codigo");
+
+		$stmt->bindParam(":codigo", $datos["codigo"], PDO::PARAM_INT);
+		$stmt->bindParam(":id_cliente", $datos["id_cliente"], PDO::PARAM_INT);
+		$stmt->bindParam(":id_vendedor", $datos["id_vendedor"], PDO::PARAM_INT);
+		$stmt->bindParam(":productos", $datos["productos"], PDO::PARAM_STR);
+		$stmt->bindParam(":impuesto", $datos["impuesto"], PDO::PARAM_STR);
+		$stmt->bindParam(":neto", $datos["neto"], PDO::PARAM_STR);
+		$stmt->bindParam(":total", $datos["total"], PDO::PARAM_STR);
+		$stmt->bindParam(":metodo_pago", $datos["metodo_pago"], PDO::PARAM_STR);
+
+		if($stmt->execute()){
+
+			return "ok";
+
+		}else{
+
+			return "error";
+		
+		}
+		$stmt = null;
+
+	}
 }
--- views/modulos/ventas.php
+++ views/modulos/ventas.php
@@ -1,59 +1,142 @@
-<!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-        <h1>
-            Administrar Ventas
-        </h1>
-        <ol class="breadcrumb">
-            <li><a href="inicio"><i class="fa fa-dashboard"></i> Inicio</a></li>
-            <li class="active">Administrar Ventas</li>
-        </ol>
-    </section>
-    <section class="content">
-        <div class="box">
-            <div class="box-header with-border">
-                <a href="crear-venta">
-                    <button class="btn btn-primary">
-                        Agregar Venta
-                    </button>
-                </a>
-            </div>
-            <div class="box-body">
-                <table class="table table-bordered table-striped dt-responsive tablas" width="100%">
-                    <thead>
-                        <tr>
-                            <th style="width: 10px;">#</th>
-                            <th>Código factura</th>
-                            <th>Cliente</th>
-                            <th>Vendedor</th>
-                            <th>Forma de pago</th>
-                            <th>Neto</th>
-                            <th>Total</th>
-                            <th>Fecha</th>
-                            <th>Acciones</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>10000112</td>
-                            <td>Juan</td>
-                            <td>Julio</td>
-                            <td>TC 1241225346</td>
-                            <td>1,000.00</td>
-                            <td>1,190.00</td>
-                            <td>2023-05-23 12:05:32</th>
-                            <td>
-                                <div class="btn-group">
-                                    <button class="btn btn-info"><i class="fa fa-print"></i></button>
-                                    <button class="btn btn-danger"><i class="fa fa-times"></i></button>
-                                </div>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    </section>
+
+  <section class="content-header">
+    
+    <h1>
+      
+      Administrar ventas
+    
+    </h1>
+
+    <ol class="breadcrumb">
+      
+      <li><a href="inicio"><i class="fa fa-dashboard"></i> Inicio</a></li>
+      
+      <li class="active">Administrar ventas</li>
+    
+    </ol>
+
+  </section>
+
+  <section class="content">
+
+    <div class="box">
+
+      <div class="box-header with-border">
+  
+        <a href="crear-venta">
+
+          <button class="btn btn-primary">
+            
+            Agregar venta
+
+          </button>
+
+        </a>
+
+      </div>
+
+      <div class="box-body">
+        
+       <table class="table table-bordered table-striped dt-responsive tablas" width="100%">
+         
+        <thead>
+         
+         <tr>
+           
+           <th style="width:10px">#</th>
+           <th>Código factura</th>
+           <th>Cliente</th>
+           <th>Vendedor</th>
+           <th>Forma de pago</th>
+           <th>Neto</th>
+           <th>Total</th> 
+           <th>Fecha</th>
+           <th>Acciones</th>
+
+         </tr> 
+
+        </thead>
+
+        <tbody>
+
+        <?php
+
+          $item = null;
+          $valor = null;
+
+          $respuesta = ControladorVentas::ctrMostrarVentas($item, $valor);
+
+          foreach ($respuesta as $key => $value) {
+           
+
+           echo '<tr>
+
+                  <td>'.($key+1).'</td>
+
+                  <td>'.$value["codigo"].'</td>';
+
+                  $itemCliente = "id";
+                  $valorCliente = $value["id_cliente"];
+
+                  $respuestaCliente = ControladorClientes::ctrMostrarClientes($itemCliente, $valorCliente);
+
+                  echo '<td>'.$respuestaCliente["nombre"].'</td>';
+
+                  $itemUsuario = "id";
+                  $valorUsuario = $value["id_vendedor"];
+
+                  $respuestaUsuario = ControladorUsuarios::ctrMostrarUsuarios($itemUsuario, $valorUsuario);
+
+                  echo '<td>'.$respuestaUsuario["nombre"].'</td>
+
+                  <td>'.$value["metodo_pago"].'</td>
+
+                  <td>$ '.number_format($value["neto"],2).'</td>
+
+                  <td>$ '.number_format($value["total"],2).'</td>
+
+                  <td>'.$value["fecha"].'</td>
+
+                  <td>
+
+                    <div class="btn-group">
+                        
+                      <button class="btn btn-info"><i class="fa fa-print"></i></button>
+
+                      <button class="btn btn-warning btnEditarVenta" idVenta="'.$value["id"].'"><i class="fa fa-pencil"></i></button>
+
+                      <button class="btn btn-danger btnEliminarVenta" idVenta="'.$value["id"].'"><i class="fa fa-times"></i></button>
+
+                    </div>  
+
+                  </td>
+
+                </tr>';
+            }
+
+        ?>
+               
+        </tbody>
+
+       </table>
+
+       <?php
+
+      //$eliminarVenta = new ControladorVentas();
+    //  $eliminarVenta -> ctrEliminarVenta();
+
+      ?>
+       
+
+      </div>
+
+    </div>
+
+  </section>
+
 </div>
+
+
+
+
